b867cc73b4b1bfc84d4fd94dc356dd08a15cc9da
Judy Fan <23451717+borby@users.noreply.github.com>
2021-07-17 17:59:51 +1000

    schemas and such

---
+++ backend/database/migrations/1626507930599_mentees.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Mentees extends BaseSchema {
+  protected tableName = 'mentees'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+      table.integer('user_id').references('id').inTable('users').notNullable()
+      //table.integer('project_id').references('id').inTable('project').notNullable()
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
