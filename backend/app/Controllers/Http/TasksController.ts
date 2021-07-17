d9332b79e08ff35ca62c86676555a9d385c49cf2
Yousif <yousef2000khalid@gmail.com>
2021-07-17 22:08:09 +1000

    user info on login + linting

--- backend/app/Controllers/Http/TasksController.ts
+++ backend/app/Controllers/Http/TasksController.ts
@@ -1,13 +1,13 @@
 // import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-import Task from "App/Models/Task"
-import TaskValidator from "App/Validators/TaskValidator"
+import Task from 'App/Models/Task'
+import TaskValidator from 'App/Validators/TaskValidator'
 
 export default class TasksController {
-    public async postTask({auth, request, response}) {
-        await auth.use('api').authenticate()
-        const input = await request.validate(TaskValidator)
-        const newProject = await Task.create(input)
-        return response.created(newProject)
-    }
+  public async postTask({ auth, request, response }) {
+    await auth.use('api').authenticate()
+    const input = await request.validate(TaskValidator)
+    const newProject = await Task.create(input)
+    return response.created(newProject)
+  }
 }
